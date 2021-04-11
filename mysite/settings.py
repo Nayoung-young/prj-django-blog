be7f9105237fe77d80979365c455a6cc271274f6
Nayoung-young <lina_ny@naver.com>
2021-04-11 21:32:24 +0900

    add searchForm

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
